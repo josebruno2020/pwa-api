b94169dd55593c7d959756a81a9983a5cfac0838
José Bruno <josebrunocampanholi@gmail.com>
2023-01-11 10:51:29 -0300

    :hammer: put certs path

--- config/websockets.php
+++ config/websockets.php
@@ -114,13 +114,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', ' /etc/letsencrypt/live/api.monintox.online/fullchain.pem'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', ' /etc/letsencrypt/live/api.monintox.online/privkey.pem'),
 
         /*
          * Passphrase for your local_cert file.
